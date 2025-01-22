638ab10e4eae62f73c4f6ceef021500d331617dd
Neyko Naydenov <naydenovneyko@gmail.com>
2025-01-22 17:17:54 +0200

    updated config

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 This is my personal configuration in Emacs that I use.
 
-
+If you'd like to use it, please create an init.el in you /home/.emacs.d and write
+(when (file-exists-p "home/.emacs.d/emacs-config/config.el")
+(load "home/emacs.d/emacs-config/config.el"))
+and place the emacs-config folder in your home directory.
+If you don't know what your home directory is you can check it using echo $HOME in your terminal.
+By default it should be "~/"
 KeyBinds :
-
+Ctrl-F to use swiper to find matches of text in the file.
+I use counsel for searching commands using "M-x".
 //TODO fill in;
 
--- config.el
+++ config.el
@@ -5,15 +5,14 @@
 (set-fringe-mode 10) ;give some breathing room
 (menu-bar-mode -1) ;disable the menu bar
 
-
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (require 'package)
+(package-initialize)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("melpa-stable" . "https://stable.melpa.org/packages/") 
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
+(unless (boundp 'package-archive-contents)
   (package-refresh-contents))
 ; initialize use package on non-linux
 (unless (package-installed-p 'use-package)
@@ -51,6 +50,17 @@
 	 ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
+(use-package counsel
+  :ensure t
+  :bind (("C-x C-f" . counsel-find-file)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-b" . counsel-ibuffer)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil))
+;;hotkeys
+
 
 (use-package doom-modeline
   :ensure t
